e430914fd0e62e594e867598dd7833c168bb4f1f
kaartiksingh23 <kaartiksingh23@gmail.com>
2020-05-12 14:59:48 +0530

    minor fix

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname));
-// app.use(express.static(__dirname + 'projects/javascript-weather-app/'));
-// app.use(express.static(__dirname + 'projects/javascript-Music-app/'));
+
+app.get('/music-app', function(req,res){
+    res.sendFile(__dirname + '/projects/javascript-Music-App/'); 
+});
+
+app.get('/weather-app', function(req,res){
+    res.sendFile(__dirname + '/projects/javascript-weather-App/'); 
+});
+
 app.listen(process.env.port || 8080);
