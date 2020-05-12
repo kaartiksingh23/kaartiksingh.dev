46221b9597e5cfbb0bfaca726a1f74a9280adb02
kaartiksingh23 <kaartiksingh23@gmail.com>
2020-05-12 11:46:41 +0530

    added routing

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
 
-//setting middleware
-app.use(express.static(__dirname)); //Serves resources from root folder
-// app.use(app.router);
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
+router.get('/weather-app',function(req,res){
+  res.sendFile(path.join(__dirname+'/projects/javascript-weather-app/index.html'));
+});
 
-var server = app.listen(8080);
+router.get('/music-app',function(req,res){
+  res.sendFile(path.join(__dirname+'/projects/javascript-Music-App/index.html'));
+});
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 8080);
