64223637727b2410006c04ffe22f62b1fe2d35f2
kaartiksingh23 <kaartiksingh23@gmail.com>
2020-05-12 13:31:23 +0530

    minor fix

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const router = express.Router();
-
-
-router.get('/weather-app',function(req,res){
-  res.sendFile(path.join(__dirname+'/projects/javascript-weather-app/index.html')); 
-  app.use(express.static(path.join(__dirname+'/projects/javascript-weather-App/')))
-});
-
-router.get('/music-app',function(req,res){
-  res.sendFile(path.join(__dirname+'/projects/javascript-Music-App/'));
-  app.use(express.static(path.join(__dirname+'/projects/javascript-Music-App/')))
-});
-
-//add the router
 
 app.use(express.static(__dirname));
-app.use('/', router);
-
+app.use('/weather-app', express.static('projects/javascript-weather-app/'));
+app.use('/music-app', express.static('projects/javascript-Music-app/'));
 app.listen(process.env.port || 8080);
