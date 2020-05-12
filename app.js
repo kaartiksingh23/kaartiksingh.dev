b52a3de1c79d6380c64f005a50cc3b29aff3b9dd
kaartiksingh23 <kaartiksingh23@gmail.com>
2020-05-12 14:20:06 +0530

    fixes

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname));
-app.use('/weather-app', express.static(__dirname + 'projects/javascript-weather-app/'));
-app.use('/music-app', express.static(__dirname + 'projects/javascript-Music-app/'));
+// app.use(express.static(__dirname + 'projects/javascript-weather-app/'));
+// app.use(express.static(__dirname + 'projects/javascript-Music-app/'));
 app.listen(process.env.port || 8080);
