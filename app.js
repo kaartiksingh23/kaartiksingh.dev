4d6c9e95cfff199dddbd51d3410b965ae028120c
kaartiksingh23 <kaartiksingh23@gmail.com>
2020-05-12 12:12:24 +0530

    minor changes to app.js

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
 
 router.get('/weather-app',function(req,res){
-  res.sendFile(path.join(__dirname+'/projects/javascript-weather-app/index.html'));
+  res.sendFile(path.join(__dirname+'/projects/javascript-weather-app/index.html')); 
+  app.use(express.static(path.join(__dirname+'/projects/javascript-weather-App/')))
 });
 
 router.get('/music-app',function(req,res){
-  res.sendFile(path.join(__dirname+'/projects/javascript-Music-App/index.html'));
+  res.sendFile(path.join(__dirname+'/projects/javascript-Music-App/'));
+  app.use(express.static(path.join(__dirname+'/projects/javascript-Music-App/')))
 });
 
 //add the router
+
+app.use(express.static(__dirname));
 app.use('/', router);
+
 app.listen(process.env.port || 8080);
