cb0e1d6a41aba6a393aeacdea13a51ad862ddb2c
kaartiksingh23 <kaartiksingh23@gmail.com>
2020-05-13 12:23:53 +0530

    added a new route

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname));
 
-app.get('/music-app', function(req,res){
-    res.sendFile(__dirname + '/projects/javascript-Music-App/'); 
+app.get("/music-app", function (req, res) {
+  res.sendFile(__dirname + "/projects/javascript-Music-App/");
 });
 
-app.get('/weather-app', function(req,res){
-    res.sendFile(__dirname + '/projects/javascript-weather-app/'); 
+app.get("/weather-app", function (req, res) {
+  res.sendFile(__dirname + "/projects/javascript-weather-app/");
+});
+
+app.get("/landing-page", function (req, res) {
+  res.sendFile(__dirname + "/projects/responsive-laptop-marketing-page/");
 });
 
 app.listen(process.env.port || 8080);
